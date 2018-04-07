2077535c44b8453522a9857fe41add0344dafc4c
viktor <viktoraju@gmail.com>
2018-04-06 19:01:15 -0600

    ADDED, new files for datatables libararies

---
+++ WebGrupo3S/Controllers/ExportarController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.xml;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.tool.xml;
+
+namespace WebGrupo3S.Controllers
+{
+    public class ExportarController : Controller
+    {
+        // GET: Exportar
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public FileResult Export(string GridHtml)
+        {
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+
+                StringReader sr = new StringReader(GridHtml);
+                Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 100f, 0f);
+                PdfWriter writer = PdfWriter.GetInstance(pdfDoc, stream);
+                pdfDoc.Open();
+                XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr);
+                pdfDoc.Close();
+                return File(stream.ToArray(), "application/pdf", "Grid.pdf");
+
+            }
+        }
+    }
+}
---
+++ WebGrupo3S/Helpers/CompleteString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebGrupo3S.Helpers
+{
+    public class CompleteString
+    {
+        public string Complete(int num, params object[] longstring) {
+
+            string parts = "";
+
+            for (int i = 0; i<longstring.Length; i++) {
+
+                if (longstring[i] == null)
+                {
+                    longstring[i] = "null";
+                }
+
+                parts = parts + longstring[i]+",";
+
+            }
+
+            Console.WriteLine(string.Concat(longstring));
+            return (parts);
+        }
+    }
+}
