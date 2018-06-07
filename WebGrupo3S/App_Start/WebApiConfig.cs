c131b731f29261464aeeb5f75ea7cbb360581ac3
viktor <viktoraju@gmail.com>
2018-06-07 17:18:31 -0600

    ADDED, WebApiConfig.cs - add new file for default routes api

---
+++ WebGrupo3S/App_Start/WebApiConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace WebGrupo3S.App_Start
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "ApiS3",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
