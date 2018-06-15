f945d4245dec6c5972b19c59c90f6794e42d1a25
viktor <viktoraju@gmail.com>
2018-06-15 13:58:03 -0600

    ADDED, ApiUsuarioController.cs - added controller to support webservice

---
+++ WebGrupo3S/Controllers/ApiUsuarioController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebGrupo3S.Models;
+
+namespace WebGrupo3S.Controllers
+{
+    public class ApiUsuarioController : ApiController
+    {
+        private SSS_PERSONASEntities db = new SSS_PERSONASEntities();
+
+        // GET: api/ApiUsuario
+        public IQueryable<Usuario> GetUsuario()
+        {
+            return db.Usuario;
+        }
+
+        // GET: api/ApiUsuario/5
+        [ResponseType(typeof(Usuario))]
+        public IHttpActionResult GetUsuario(short id)
+        {
+            Usuario usuario = db.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(usuario);
+        }
+
+        // PUT: api/ApiUsuario/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutUsuario(short id, Usuario usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != usuario.us_empresa)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(usuario).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsuarioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ApiUsuario
+        [ResponseType(typeof(Usuario))]
+        public IHttpActionResult PostUsuario(Usuario usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Usuario.Add(usuario);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (UsuarioExists(usuario.us_empresa))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = usuario.us_empresa }, usuario);
+        }
+
+        // DELETE: api/ApiUsuario/5
+        [ResponseType(typeof(Usuario))]
+        public IHttpActionResult DeleteUsuario(short id)
+        {
+            Usuario usuario = db.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            db.Usuario.Remove(usuario);
+            db.SaveChanges();
+
+            return Ok(usuario);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UsuarioExists(short id)
+        {
+            return db.Usuario.Count(e => e.us_empresa == id) > 0;
+        }
+    }
+}
