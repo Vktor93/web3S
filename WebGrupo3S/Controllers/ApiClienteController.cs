b590897bb3a83749eff83b2e755d910e28fa635b
viktor <viktoraju@gmail.com>
2018-06-15 13:50:34 -0600

    ADDED, ApiClienteController.cs - added new controller to support webservice

---
+++ WebGrupo3S/Controllers/ApiClienteController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebGrupo3S.Models;
+
+namespace WebGrupo3S.Controllers
+{
+    public class ApiClienteController : ApiController
+    {
+        private SSS_PERSONASEntities db = new SSS_PERSONASEntities();
+
+        // GET: api/ApiCliente
+        public IQueryable<Cliente> GetCliente()
+        {
+            return db.Cliente;
+        }
+
+        // GET: api/ApiCliente/5
+        [ResponseType(typeof(Cliente))]
+        public IHttpActionResult GetCliente(short id)
+        {
+            Cliente cliente = db.Cliente.Find(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cliente);
+        }
+
+        // PUT: api/ApiCliente/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCliente(short id, Cliente cliente)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != cliente.cl_empresa)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(cliente).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClienteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ApiCliente
+        [ResponseType(typeof(Cliente))]
+        public IHttpActionResult PostCliente(Cliente cliente)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Cliente.Add(cliente);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (ClienteExists(cliente.cl_empresa))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = cliente.cl_empresa }, cliente);
+        }
+
+        // DELETE: api/ApiCliente/5
+        [ResponseType(typeof(Cliente))]
+        public IHttpActionResult DeleteCliente(short id)
+        {
+            Cliente cliente = db.Cliente.Find(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            db.Cliente.Remove(cliente);
+            db.SaveChanges();
+
+            return Ok(cliente);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ClienteExists(short id)
+        {
+            return db.Cliente.Count(e => e.cl_empresa == id) > 0;
+        }
+    }
+}
