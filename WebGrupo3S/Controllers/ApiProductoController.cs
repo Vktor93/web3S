7e13c07be35191b385f52fd26f0b83c23ca6db58
viktor <viktoraju@gmail.com>
2018-06-18 10:33:26 -0600

    ADDED, ApiProductoController.cs - added controller to support webservice

---
+++ WebGrupo3S/Controllers/ApiProductoController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebGrupo3S.Models;
+
+namespace WebGrupo3S.Controllers
+{
+    public class ApiProductoController : ApiController
+    {
+        private SSS_OPERACIONEntities db = new SSS_OPERACIONEntities();
+
+        // GET: api/ApiProducto
+        public IQueryable<Producto> GetProducto()
+        {
+            return db.Producto;
+        }
+
+        // GET: api/ApiProducto/5
+        [ResponseType(typeof(Producto))]
+        public IHttpActionResult GetProducto(short id)
+        {
+            Producto producto = db.Producto.Find(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(producto);
+        }
+
+        // PUT: api/ApiProducto/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProducto(short id, Producto producto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != producto.pr_empresa)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(producto).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ApiProducto
+        [ResponseType(typeof(Producto))]
+        public IHttpActionResult PostProducto(Producto producto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Producto.Add(producto);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (ProductoExists(producto.pr_empresa))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = producto.pr_empresa }, producto);
+        }
+
+        // DELETE: api/ApiProducto/5
+        [ResponseType(typeof(Producto))]
+        public IHttpActionResult DeleteProducto(short id)
+        {
+            Producto producto = db.Producto.Find(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            db.Producto.Remove(producto);
+            db.SaveChanges();
+
+            return Ok(producto);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProductoExists(short id)
+        {
+            return db.Producto.Count(e => e.pr_empresa == id) > 0;
+        }
+    }
+}
