03cfa6a80ba30cedef6942119a618b64a9204faa
viktor <viktoraju@gmail.com>
2018-06-22 19:32:09 -0600

    ADDED, ArqueoCajasController.cs / added controller to support arqueo caja

---
+++ WebGrupo3S/Controllers/ArqueoCajasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebGrupo3S.Models;
+
+namespace WebGrupo3S.Controllers
+{
+    public class ArqueoCajasController : Controller
+    {
+        private SSS_OPERACIONEntities db = new SSS_OPERACIONEntities();
+
+        // GET: ArqueoCajas
+        public ActionResult Index()
+        {
+            return View(db.ArqueoCaja.ToList());
+        }
+
+        // GET: ArqueoCajas/Details/5
+        public ActionResult Details(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ArqueoCaja arqueoCaja = db.ArqueoCaja.Find(id);
+            if (arqueoCaja == null)
+            {
+                return HttpNotFound();
+            }
+            return View(arqueoCaja);
+        }
+
+        // GET: ArqueoCajas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ArqueoCajas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "aq_empresa,aq_IdCierreCaja,aq_IdArqueoCaja,aq_Moneda,aq_PresentacionMoneda,aq_Denominacion,aq_Cantidad,aq_MontoTotal,aq_fechaing,aq_fechamod,aq_usuarioing,aq_usuariomod,aq_maquinaing,aq_maquinamod,aq_estado,aq_timestamp")] ArqueoCaja arqueoCaja)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ArqueoCaja.Add(arqueoCaja);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(arqueoCaja);
+        }
+
+        // GET: ArqueoCajas/Edit/5
+        public ActionResult Edit(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ArqueoCaja arqueoCaja = db.ArqueoCaja.Find(id);
+            if (arqueoCaja == null)
+            {
+                return HttpNotFound();
+            }
+            return View(arqueoCaja);
+        }
+
+        // POST: ArqueoCajas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "aq_empresa,aq_IdCierreCaja,aq_IdArqueoCaja,aq_Moneda,aq_PresentacionMoneda,aq_Denominacion,aq_Cantidad,aq_MontoTotal,aq_fechaing,aq_fechamod,aq_usuarioing,aq_usuariomod,aq_maquinaing,aq_maquinamod,aq_estado,aq_timestamp")] ArqueoCaja arqueoCaja)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(arqueoCaja).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(arqueoCaja);
+        }
+
+        // GET: ArqueoCajas/Delete/5
+        public ActionResult Delete(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ArqueoCaja arqueoCaja = db.ArqueoCaja.Find(id);
+            if (arqueoCaja == null)
+            {
+                return HttpNotFound();
+            }
+            return View(arqueoCaja);
+        }
+
+        // POST: ArqueoCajas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(short id)
+        {
+            ArqueoCaja arqueoCaja = db.ArqueoCaja.Find(id);
+            db.ArqueoCaja.Remove(arqueoCaja);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
