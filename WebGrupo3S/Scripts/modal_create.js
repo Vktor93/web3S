6d81817d7cd0887e41f9955c7cf988578e3ebf4b
viktor <viktoraju@gmail.com>
2018-08-17 08:45:25 -0600

    UPDATE, add function to click edit button

--- WebGrupo3S/Scripts/modal_create.js
+++ WebGrupo3S/Scripts/modal_create.js
@@ -1,7 +1,40 @@
 ﻿$(document).ready(function () {
 
-    $("#btn_crear").click(function () {
+    $("#btn-mostrar").click(function () {
 
+        $.ajax({
+            url: './perfilView',
+            type: "POST",
+            dataType: 'json',            
+        })
+        .done(function (response) {
+            $(".modal").html(response.PartialView);
+            //console.log(response.PartialView);            
+        })
+        .fail(function (response) {
+            console.log(response);
+        });
+        
+        $("#modal_create").modal();
+    });
+
+
+    $("#datos .update").click(function () {
+        var id = $(this).closest("tr").find("td").eq(0).html();
+        console.log(id);
+
+
+        $.ajax()
     });
 
 });
+
+function closeModal() {
+    $("#modal_create").modal("hide");
+}
+
+function editProfile() {
+    $("#btn-editar").click(function () {
+
+    });
+}
