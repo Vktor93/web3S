aaa2170886fc69362600682d88c6fb241c664805
viktor <viktoraju@gmail.com>
2018-08-20 15:57:23 -0600

    UPDATE, add jsonObject variable

--- WebGrupo3S/Scripts/modal_create.js
+++ WebGrupo3S/Scripts/modal_create.js
@@ -1,7 +1,7 @@
 ﻿$(document).ready(function () {
 
     $("#btn-mostrar").click(function () {
-
+    
         $.ajax({
             url: './perfilView',
             type: "POST",
@@ -13,28 +13,50 @@
         })
         .fail(function (response) {
             console.log(response);
+            return false;
         });
         
         $("#modal_create").modal();
-    });
-
-
-    $("#datos .update").click(function () {
-        var id = $(this).closest("tr").find("td").eq(0).html();
-        console.log(id);
-
-
-        $.ajax()
-    });
+    });    
 
 });
 
+//FUNCION PARA CERRAR MODAL
 function closeModal() {
     $("#modal_create").modal("hide");
+    $("#nombre").val("");
+    $("#descripcion").val("");
 }
 
-function editProfile() {
-    $("#btn-editar").click(function () {
 
+//FUNCION PARA LEVANTAR VISTA DE EDITAR PERFIL
+function viewUpdate(link) {
+                  
+    var row = link.parentNode.parentNode;
+    var id = parseInt(row.getElementsByTagName("td")[0].innerHTML);
+    //var id = parseInt(link.closest("tr").find("td")[0].innerHTML);
+    console.log(id);
+
+    //var perfil = new Object();
+
+    //perfil.pf_nomPerfil = $("#nombre").val();
+    //perfil.pf_descPerfil = $("#descripcion").val();
+
+    //console.log(perfil)
+
+    $.ajax({
+        url: './updateView',
+        type: "POST",
+        data: { id: id} ,
+        dataType: 'json',
+    })
+    .done(function (response) {
+        $(".modal").html(response.PartialView);
+        //console.log(response.PartialView);            
+    })
+    .fail(function (response) {
+        console.log(response);
     });
+
+    $("#modal_create").modal();
 }
