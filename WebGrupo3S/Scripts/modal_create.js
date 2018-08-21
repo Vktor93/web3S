3d2cd2bf95a4fd09696a51518eaca03a25c197bb
viktor <viktoraju@gmail.com>
2018-08-21 13:14:02 -0600

    UPDATE, add function to support modal for delete perfil view

--- WebGrupo3S/Scripts/modal_create.js
+++ WebGrupo3S/Scripts/modal_create.js
@@ -60,3 +60,35 @@ function viewUpdate(link) {
 
     $("#modal_create").modal();
 }
+
+//FUNCION PARA LEVANTAR VISTA DE EDITAR PERFIL
+function viewDelete(link) {
+                  
+    var row = link.parentNode.parentNode;
+    var id = parseInt(row.getElementsByTagName("td")[0].innerHTML);
+    //var id = parseInt(link.closest("tr").find("td")[0].innerHTML);
+    console.log(id);
+
+    //var perfil = new Object();
+
+    //perfil.pf_nomPerfil = $("#nombre").val();
+    //perfil.pf_descPerfil = $("#descripcion").val();
+
+    //console.log(perfil)
+
+    $.ajax({
+        url: './deleteView',
+        type: "POST",
+        data: { id: id} ,
+        dataType: 'json',
+    })
+    .done(function (response) {
+        $(".modal").html(response.PartialView);
+        //console.log(response.PartialView);            
+    })
+    .fail(function (response) {
+        console.log(response);
+    });
+
+    $("#modal_create").modal();
+}
